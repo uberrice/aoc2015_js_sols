563580cb712ebe79e420628863078bdcd5f9994d
uberrice <overrice@mail.ru>
2019-04-16 09:46:38 +0200

    day 11 works, both parts

---
+++ aoc15d11.ts
@@ -0,0 +1,35 @@
+var input : string = "cqjxjnds";
+
+function aInc(ip: string): string{
+    if(ip.slice(-1).match(/[hnk]/)){
+        return ip.slice(0, ip.length-1) + String.fromCharCode(2+ip.charCodeAt(ip.length-1));
+    } else if(ip.slice(-1) != "z"){
+        return ip.slice(0, ip.length-1) + String.fromCharCode(1+ip.charCodeAt(ip.length-1));
+    }
+    else{
+        return (aInc(ip.slice(0,-1)) + "a"); //sweet sweet recursion
+    }
+}
+
+function pwFinder(ip : string): string{
+    //todo: check double chars (two of them)
+    while(true){
+        if(ip.match(/(.)\1.*(.)\2/)){ //check if double chars exist
+            for (let index = 0; index < ip.length-2; index++) {
+                if((ip.charCodeAt(index)+2) === (ip.charCodeAt(index+1)+1) && (ip.charCodeAt(index+1)+1) === ip.charCodeAt(index+2)) { //check for increasing chars
+                    if(!ip.match(/[iol]/)){
+                        return ip;
+
+                    }
+                }  
+            }
+        }
+        ip = aInc(ip);
+        //console.log(ip);
+
+    }
+    
+}
+
+console.log("part 1 sol: " + pwFinder(input));
+console.log("part 2 sol: " + pwFinder(aInc(pwFinder(input))));
