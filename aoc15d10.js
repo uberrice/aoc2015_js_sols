1d38a27f90bea8a8176d80500f5cf731851963a2
uberrice <overrice@mail.ru>
2019-04-16 08:40:07 +0200

    unrecursified d10, runs now

--- aoc15d10.js
+++ aoc15d10.js
@@ -1,26 +1,30 @@
 var input = "1113222113";
 var output = "";
 function lookAndSay(s){
-    let occur = 1;
-    for (let index = 0; index < s.length; index++) {
-        let c = s[index];
-        let c2 = s[index+1];
-        if((c === c2) && (c2 !=undefined)){
-            occur++;
-        } else{
-            output+= occur;
-            output+= c;
-            lookAndSay(s.slice(index+1));
-            break;
-        }
+    while(s.length != 0){
+        let occur = 1;
+        for (let index = 0; index < s.length; index++) {
+            let c = s[index];
+            let c2 = s[index+1];
+            if((c === c2) && (c2 !=undefined)){
+                occur++;
+            } else{
+                output+= occur;
+                output+= c;
+                s = s.slice(index+1);
+                break;
+            }
+        }  
     }
 }
+
 lookAndSay(input);
-for (let i = 0; i < 40; i++) {
+
+for (let i = 1; i < 40; i++) {
     let o2 = output;
     output = "";
     lookAndSay(o2);
 }
 //doesn't work because it's too recursive :(
 
-console.log(output);
+console.log(output.length);
